1d5179ad49e678fbd0ad1705f36cbe6889ce0f2d
German De León <141041025+gerdeleon@users.noreply.github.com>
2023-08-13 17:36:52 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,1 +1,8 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const styleBtn = document.getElementById('styleBtn');
+  
+    styleBtn.addEventListener('click', function() {
+      alert('Hola! Soy el div');
+    });
+  });
+  
